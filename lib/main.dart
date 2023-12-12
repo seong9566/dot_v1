5bef4ec60f8a431b4a51ea9175e901e92df0283a
seong9566 <dlgustjd9566@gmail.com>
2023-12-12 23:44:14 +0900

    프로젝트 생성

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text("New doe"),
+        ),
+        body: const Center(child: Text("body")),
+      ),
+    );
+  }
+}
